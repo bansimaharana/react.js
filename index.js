7605b7a81ed2f9d8a8fdb61a164ab0186b370c1a
bansimaharana <bansimaharana12415@gmail.com>
2024-04-25 22:18:50 +0530

    fift commit

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ console.log("React",ReactDOM);
 const root=document.getElementById("root")
 
 const heading1= React.createElement("h1",{class:"heading1"},"heading1");
-const container1= React.createElement("div",{class:"container1"},heading1);
-console.log(container1)
+const container1= React.createElement("div",{class:"container1",style:{backgroundColor:"red"}},heading1);
+
 
 const heading2= React.createElement("h3",{class:"heading2"},"heading2");
-const container2= React.createElement("div",{class:"container2"},heading2);
+const container2= React.createElement("div",{class:"container2",style:{backgroundColor:"yellow"}},heading2);
 
 const rootElement= ReactDOM.createRoot(root);
 
