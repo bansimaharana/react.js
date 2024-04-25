9bc91ab74548fae2fe777a7330adbe75017e6568
bansimaharana <bansimaharana12415@gmail.com>
2024-04-25 20:41:55 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 console.log("React",React);
 console.log("React",ReactDOM);
+
+const root=document.getElementById("root")
+
+const heading1= React.createElement("h1",{class:"heading1"},"heading1");
+const container1= React.createElement("div",{class:"container1"},heading1);
+console.log(container1)
+
+const heading2= React.createElement("h3",{class:"heading2"},"heading2");
+const container2= React.createElement("div",{class:"container2"},heading2);
+
+const rootElement= ReactDOM.createRoot(root);
+
+rootElement.render([container1,container2]);
