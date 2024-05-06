0770c36117a1c927a1f5cad3f8037fb80375fd2e
bansimaharana <bansimaharana12415@gmail.com>
2024-05-06 16:44:46 +0530

    sixth commit

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 console.log("React",React);
 console.log("React",ReactDOM);
 
 const root=document.getElementById("root")
 
-const heading1= React.createElement("h1",{class:"heading1"},"heading1");
+const heading1= React.createElement("h1",{class:"heading1"},"heading2 hello react");
 const container1= React.createElement("div",{class:"container1",style:{backgroundColor:"red"}},heading1);
 
 
-const heading2= React.createElement("h3",{class:"heading2"},"heading2");
+const heading2= React.createElement("h3",{class:"heading2"},"heading3 hello react.js");
 const container2= React.createElement("div",{class:"container2",style:{backgroundColor:"yellow"}},heading2);
 
 const rootElement= ReactDOM.createRoot(root);
